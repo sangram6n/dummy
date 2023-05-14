1d8277177511fe9855fd9acfe78f6e9c4dc418a5
Sangram Mishra <72312540+sangram6n@users.noreply.github.com>
2023-05-14 12:56:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 x = "hello world"
 
-with open("file.txt","a") as f:
-    m = f.write(x)
-    f.close()
+try:
+    SOME_SECRET = os.environ["SOME_SECRET"]
+except KeyError:
+    SOME_SECRET = "Token not available!"
+    
+    
+if __name__ == "__main__":
+    with open("file.txt","a") as f:
+        m = f.write(x)
+        f.close()
 
 
 
